d810724d62334e907b60095770dd0ff1a548c3b1
Giorgi Kalandadze <giorgikalandadze24@gmail.com>
2024-04-19 18:55:50 +0400

    Init express server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+if (!process.env.NODE_ENV) {
+    require('dotenv').config();
+}
+const express = require('express')
+const {RESULT_CODES, RESULT_STATUSES} = require('./src/constants');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        resultCode: RESULT_CODES.SUCCESS,
+        resultStatus: RESULT_STATUSES.SUCCESS,
+
+    })
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
+});
